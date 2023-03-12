96a76d58b5bfe318fb244889bc4448e02f4e3aff
Johannes Pfleghar <pixelsuppe@gmail.com>
2023-03-12 15:08:11 +0100

    [world-5] added filterbar style, functionality + installed prettifier

--- whereintheworld/src/app/country-list/components/country-list-item/country-list-item.component.ts
+++ whereintheworld/src/app/country-list/components/country-list-item/country-list-item.component.ts
@@ -1,12 +1,11 @@
-import {Component, Input, OnInit} from '@angular/core';
-import {Country} from '../../models/Country';
+import { Component, Input } from '@angular/core';
+import { Country } from '../../models/Country';
 
 @Component({
   selector: 'app-country-list-item',
   templateUrl: './country-list-item.component.html',
-  styleUrls: ['./country-list-item.component.scss']
+  styleUrls: ['./country-list-item.component.scss'],
 })
 export class CountryListItemComponent {
   @Input() country: Country;
-
 }
